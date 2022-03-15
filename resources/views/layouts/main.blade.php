0b7d8375e7e780c7f2e7e473b5c863a33afdd34c
Yudhistiro Arie <yudhistiroarie@gmail.com>
2022-03-15 18:55:31 +0700

    20220315_1855

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -190,60 +190,17 @@
                                 Tentang Kami
                             </h2>
                             
+                            <br>
+                            
                             <p>
                                 Perusahaan kami bergerak di bidang <b>IT</b> dan <b>Creative Design</b>. Kami bekerja keras untuk membantu memecahkan masalah klien secara efektif dan kreatif.
+                                
+                                <br>
+                                <br>
+                                
+                                Dengan tim yang profesional, kreatif dan berkomitmen, kami siap membantu mewujudkan ide dan impian Anda untuk masa depan bisnis Anda.
                             </p>
-                            
-                            <div class="icon-box" data-aos="fade-up" data-aos-delay="100" hidden>
-                                <div class="icon">
-                                    <i class="bi bi-briefcase"></i>
-                                </div>
-                                
-                                <h4 class="title">
-                                    <a href="">
-                                        Eiusmod Tempor
-                                    </a>
-                                </h4>
-                                
-                                <p class="description">
-                                    Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi
-                                </p>
-                            </div>
-                            
-                            <div class="icon-box" data-aos="fade-up" data-aos-delay="200" hidden>
-                                <div class="icon">
-                                    <i class="bi bi-card-checklist"></i>
-                                </div>
-                                
-                                <h4 class="title">
-                                    <a href="">
-                                        Magni Dolores
-                                    </a>
-                                </h4>
-                                
-                                <p class="description">
-                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                                </p>
-                            </div>
-                            
-                            <div class="icon-box" data-aos="fade-up" data-aos-delay="300" hidden>
-                                <div class="icon">
-                                    <i class="bi bi-binoculars"></i>
-                                </div>
-                                
-                                <h4 class="title">
-                                    <a href="">
-                                        Dolor Sitema
-                                    </a>
-                                </h4>
-                                
-                                <p class="description">
-                                    Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata
-                                </p>
-                            </div>
                         </div>
-                        
-                        <div class="col-lg-6 background order-lg-2 order-1" data-aos="fade-left" data-aos-delay="100" hidden></div>
                     </div>
                 </div>
             </section><!-- End About Section -->
@@ -298,16 +255,57 @@
                 <div class="container" data-aos="fade-up">
                     <div class="section-header">
                         <h3 class="section-title">
-                            Services
+                            Layanan Kami
                         </h3>
                         
-                        <p class="section-description">
+                        <br>
+                        
+                        <p class="section-description" hidden>
                             Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
                         </p>
                     </div>
                     
-                    <div class="row">
-                        <div class="col-lg-4 col-md-6" data-aos="zoom-in">
+                    <div class="row justify-content-center">
+                        @php
+                        // IT Aplikasi
+                        // Desain Grafis
+                        // Desain Periklanan
+                        // Desain Media Sosial
+                        // Desain Web
+                        // Ilustrasi
+                        // Desain Produk, Kemasan, Logo, Brand Identity
+                        // Video Editing
+                        // Branding
+                        $services = [
+                            [
+                                "name" => "CREATIVE DEVELOPMENT",
+                                "desc" => 'CREATIVE CONCEPTS<br>UX DESIGN &amp; REVIEW<br>UI DESIGN<br>OUTDOOR MEDIA',
+                            ],
+                            [
+                                "name" => "TECH DEVELOPMENT",
+                                "desc" => 'FRONT-END/BACK-END<br>INTERACTIVE<br>LARAVEL/PHP<br>SITECORE<br>NATIVE MOBILE APPS',
+                            ],
+                            [
+                                "name" => "DIGITAL MARKETING",
+                                "desc" => 'SEM/SEO<br></span>SOCIAL MEDIA MANAGEMENT<br>SOCIAL MEDIA MARKETING<br>BRANDING COMPANY',
+                            ],
+                        ];
+                        foreach($services as $items) {
+                            echo '<div class="col-lg-4 col-md-6" data-aos="zoom-in">
+                                    <div class="box">
+                                        <h4 class="title">
+                                            ' . $items["name"] . '
+                                        </h4>
+                                        
+                                        <p class="description">
+                                            ' . $items["desc"] . '
+                                        </p>
+                                    </div>
+                                </div>';
+                        }
+                        @endphp
+                        
+                        <div class="col-lg-4 col-md-6" data-aos="zoom-in" hidden>
                             <div class="box">
                                 <div class="icon">
                                     <a href="">
@@ -323,106 +321,6 @@
                                 
                                 <p class="description">
                                     Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident
-                                </p>
-                            </div>
-                        </div>
-                        
-                        <div class="col-lg-4 col-md-6" data-aos="zoom-in">
-                            <div class="box">
-                                <div class="icon">
-                                    <a href="">
-                                        <i class="bi bi-card-checklist"></i>
-                                    </a>
-                                </div>
-                                
-                                <h4 class="title">
-                                    <a href="">
-                                        Dolor Sitema
-                                    </a>
-                                </h4>
-                                
-                                <p class="description">
-                                    Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata
-                                </p>
-                            </div>
-                        </div>
-                        
-                        <div class="col-lg-4 col-md-6" data-aos="zoom-in">
-                            <div class="box">
-                                <div class="icon">
-                                    <a href="">
-                                        <i class="bi bi-bar-chart"></i>
-                                    </a>
-                                </div>
-                                
-                                <h4 class="title">
-                                    <a href="">
-                                        Sed ut perspiciatis
-                                    </a>
-                                </h4>
-                                
-                                <p class="description">
-                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur
-                                </p>
-                            </div>
-                        </div>
-                        
-                        <div class="col-lg-4 col-md-6" data-aos="zoom-in">
-                            <div class="box">
-                                <div class="icon">
-                                    <a href="">
-                                        <i class="bi bi-binoculars"></i>
-                                    </a>
-                                </div>
-                                
-                                <h4 class="title">
-                                    <a href="">
-                                        Magni Dolores
-                                    </a>
-                                </h4>
-                                
-                                <p class="description">
-                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                                </p>
-                            </div>
-                        </div>
-                        
-                        <div class="col-lg-4 col-md-6" data-aos="zoom-in">
-                            <div class="box">
-                                <div class="icon">
-                                    <a href="">
-                                        <i class="bi bi-brightness-high"></i>
-                                    </a>
-                                </div>
-                                
-                                <h4 class="title">
-                                    <a href="">
-                                        Nemo Enim
-                                    </a>
-                                </h4>
-                                
-                                <p class="description">
-                                    At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque
-                                </p>
-                            </div>
-                        </div>
-                        
-                        <div class="col-lg-4 col-md-6" data-aos="zoom-in">
-                            <div class="box">
-                                <div class="icon">
-                                    <a href="">
-                                        <i class="bi bi-calendar4-week"></i>
-                                    </a>
-                                </div>
-                                
-                                <h4 class="title">
-                                    <a href="">
-                                        Eiusmod Tempor
-                                    </a>
-                                </h4>
-                                
-                                <p class="description">
-                                    Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi
                                 </p>
                             </div>
                         </div>
